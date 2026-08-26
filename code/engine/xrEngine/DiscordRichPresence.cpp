fdce2975d28c4077cbdc96d9fb5d8dd4d1bd02e4
Infernis <bohdan.kyslyi.13@gmail.com>
2026-08-26 18:05:54 +0300

    Додавання відображення рендеру

--- code/engine/xrEngine/DiscordRichPresence.cpp
+++ code/engine/xrEngine/DiscordRichPresence.cpp
@@ -42,12 +42,21 @@ xr_string ToDiscordUTF8(LPCSTR source) {
 
 } // namespace
 
+extern int g_current_renderer;
+
 ENGINE_API CDiscordRichPresence g_discord;
 
 CDiscordRichPresence::CDiscordRichPresence()
-    : m_initialized(false), m_show_playtime(true), m_show_location(true), m_start_timestamp(0) {
+    : m_initialized(false), m_show_playtime(true), m_show_location(true), m_show_renderer(true),
+      m_renderer_id(-1), m_start_timestamp(0) {
     m_details[0] = '\0';
     m_state[0] = '\0';
+
+    m_large_image_key[0] = '\0';
+    m_large_image_text[0] = '\0';
+
+    m_renderer_image_key[0] = '\0';
+    m_renderer_image_text[0] = '\0';
 }
 
 void CDiscordRichPresence::Initialize() {
@@ -77,6 +86,18 @@ void CDiscordRichPresence::Initialize() {
 
     m_show_playtime = READ_IF_EXISTS(pSettings, r_bool, "discord_rpc", "show_playtime", true);
     m_show_location = READ_IF_EXISTS(pSettings, r_bool, "discord_rpc", "show_location", true);
+    m_show_renderer = READ_IF_EXISTS(pSettings, r_bool, "discord_rpc", "show_renderer", true);
+
+    LPCSTR large_image_key = READ_IF_EXISTS(pSettings, r_string, "discord_rpc", "large_image", "");
+
+    LPCSTR large_image_text =
+        READ_IF_EXISTS(pSettings, r_string, "discord_rpc", "large_image_text", "Infernis Engine");
+
+    xr_strcpy(m_large_image_key, sizeof(m_large_image_key), large_image_key);
+
+    const xr_string utf8_large_image_text = ToDiscordUTF8(large_image_text);
+
+    xr_strcpy(m_large_image_text, sizeof(m_large_image_text), utf8_large_image_text.c_str());
 
     DiscordEventHandlers handlers{};
 
@@ -107,6 +128,56 @@ void CDiscordRichPresence::SetLocationStatus(LPCSTR location_name) {
     SetStatus(location_name, "Exploring the Zone");
 }
 
+void CDiscordRichPresence::UpdateRendererInfo() {
+    m_renderer_id = g_current_renderer;
+
+    m_renderer_image_key[0] = '\0';
+    m_renderer_image_text[0] = '\0';
+
+    if (!m_show_renderer)
+        return;
+
+    LPCSTR image_setting = nullptr;
+    LPCSTR renderer_text = nullptr;
+
+    switch (g_current_renderer) {
+    case 1:
+        image_setting = "renderer_dx9_image";
+        renderer_text = "DirectX 9 - Renderer R1";
+        break;
+
+    case 2:
+        image_setting = "renderer_dx9_image";
+        renderer_text = "DirectX 9 - Renderer R2";
+        break;
+
+    case 3:
+        image_setting = "renderer_dx10_image";
+        renderer_text = "DirectX 10 - Renderer R3";
+        break;
+
+    case 4:
+        image_setting = "renderer_dx11_image";
+        renderer_text = "DirectX 11 - Renderer R4";
+        break;
+
+    default:
+        Msg("! Discord RPC: unknown renderer [%d]", g_current_renderer);
+        return;
+    }
+
+    LPCSTR image_key = READ_IF_EXISTS(pSettings, r_string, "discord_rpc", image_setting, "");
+
+    xr_strcpy(m_renderer_image_key, sizeof(m_renderer_image_key), image_key);
+
+    const xr_string utf8_renderer_text = ToDiscordUTF8(renderer_text);
+
+    xr_strcpy(m_renderer_image_text, sizeof(m_renderer_image_text), utf8_renderer_text.c_str());
+
+    Msg("* Discord RPC: renderer [%s], image [%s]", renderer_text,
+        m_renderer_image_key[0] ? m_renderer_image_key : "-");
+}
+
 void CDiscordRichPresence::SetStatus(LPCSTR details, LPCSTR state) {
     if (!m_initialized)
         return;
@@ -115,19 +186,22 @@ void CDiscordRichPresence::SetStatus(LPCSTR details, LPCSTR state) {
     LPCSTR safe_state = state ? state : "";
 
     const xr_string utf8_details = ToDiscordUTF8(safe_details);
-
     const xr_string utf8_state = ToDiscordUTF8(safe_state);
 
+    const bool renderer_changed = m_renderer_id != g_current_renderer;
+
+    if (renderer_changed)
+        UpdateRendererInfo();
+
     //
-    // Compare already converted UTF-8 strings.
+    // Do not send an identical presence unless the renderer changed.
     //
-    if (xr_strcmp(m_details, utf8_details.c_str()) == 0 &&
+    if (!renderer_changed && xr_strcmp(m_details, utf8_details.c_str()) == 0 &&
         xr_strcmp(m_state, utf8_state.c_str()) == 0) {
         return;
     }
 
     xr_strcpy(m_details, sizeof(m_details), utf8_details.c_str());
-
     xr_strcpy(m_state, sizeof(m_state), utf8_state.c_str());
 
     DiscordRichPresence presence{};
@@ -137,6 +211,16 @@ void CDiscordRichPresence::SetStatus(LPCSTR details, LPCSTR state) {
     presence.state = m_state[0] ? m_state : nullptr;
 
     presence.startTimestamp = m_show_playtime ? m_start_timestamp : 0;
+
+    presence.largeImageKey = m_large_image_key[0] ? m_large_image_key : nullptr;
+
+    presence.largeImageText =
+        m_large_image_key[0] && m_large_image_text[0] ? m_large_image_text : nullptr;
+
+    presence.smallImageKey = m_renderer_image_key[0] ? m_renderer_image_key : nullptr;
+
+    presence.smallImageText =
+        m_renderer_image_key[0] && m_renderer_image_text[0] ? m_renderer_image_text : nullptr;
 
     Discord_UpdatePresence(&presence);
 
@@ -157,6 +241,7 @@ void CDiscordRichPresence::Shutdown() {
 
     m_initialized = false;
     m_start_timestamp = 0;
+    m_renderer_id = -1;
 
     Msg("* Discord RPC: shutdown");
 }
--- code/engine/xrEngine/DiscordRichPresence.h
+++ code/engine/xrEngine/DiscordRichPresence.h
@@ -15,14 +15,26 @@ public:
     bool IsInitialized() const { return m_initialized; }
 
 private:
+    void UpdateRendererInfo();
+
+private:
     bool m_initialized;
     bool m_show_playtime;
     bool m_show_location;
+    bool m_show_renderer;
+
+    int m_renderer_id;
 
     s64 m_start_timestamp;
 
     string128 m_details;
     string128 m_state;
+
+    string64 m_large_image_key;
+    string128 m_large_image_text;
+
+    string64 m_renderer_image_key;
+    string128 m_renderer_image_text;
 };
 
 extern ENGINE_API CDiscordRichPresence g_discord;
