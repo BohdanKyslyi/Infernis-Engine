7cc30a9c3bc61a45825a8ce373bfbe5942026077
Infernis <bohdan.kyslyi.13@gmail.com>
2026-08-27 22:57:30 +0300

    Фінальні зміни weapon_loadout
    1) Оптимізація charachers_desc_general
    2) Підтримка глушників, прицілів і підствольників

--- code/engine/xrGame/alife_object.cpp
+++ code/engine/xrGame/alife_object.cpp
@@ -12,118 +12,48 @@
 #include "xrServer_Objects_ALife_Items.h"
 
 namespace {
-LPCSTR find_loadout_parameter(LPCSTR value, LPCSTR name) {
-    if (!value || !name || !xr_strlen(name))
-        return NULL;
-
-    const u32 name_length = xr_strlen(name);
-    LPCSTR parameter = strstr(value, name);
-
-    while (parameter) {
-        if (parameter == value || parameter[-1] == ',' || parameter[-1] == ' ' ||
-            parameter[-1] == '\t') {
-            return parameter + name_length;
-        }
-
-        parameter = strstr(parameter + name_length, name);
-    }
-
-    return NULL;
-}
-
-bool has_loadout_flag(LPCSTR value, LPCSTR flag) {
-    if (!value || !flag || !xr_strlen(flag))
-        return false;
-
-    const u32 flag_length = xr_strlen(flag);
-    LPCSTR position = strstr(value, flag);
-
-    while (position) {
-        const bool valid_start =
-            position == value || position[-1] == ',' || position[-1] == ' ' ||
-            position[-1] == '\t';
-        const char next = position[flag_length];
-        const bool valid_end =
-            !next || next == ',' || next == ' ' || next == '\t';
-
-        if (valid_start && valid_end)
-            return true;
-
-        position = strstr(position + flag_length, flag);
-    }
-
-    return false;
-}
-
 struct SSpawnLoadoutEntry {
     xr_string section;
     float weight;
     float condition;
     u32 ammo_count;
     s32 ammo_type;
-    u32 drop_ammo_min;
-    u32 drop_ammo_max;
-    bool custom_drop_ammo;
-    bool keep_ammo;
     bool scope;
     bool silencer;
     bool launcher;
 
     SSpawnLoadoutEntry(LPCSTR item_section, LPCSTR value)
         : section(item_section ? item_section : ""), weight(1.f), condition(1.f), ammo_count(1),
-          ammo_type(0), drop_ammo_min(0), drop_ammo_max(0), custom_drop_ammo(false),
-          keep_ammo(false), scope(false), silencer(false), launcher(false) {
+          ammo_type(0), scope(false), silencer(false), launcher(false) {
         if (!value || !xr_strlen(value))
             return;
 
-        LPCSTR parameter = find_loadout_parameter(value, "weight=");
+        LPCSTR parameter = strstr(value, "weight=");
         if (parameter)
-            weight = (float)atof(parameter);
+            weight = (float)atof(parameter + 7);
 
-        parameter = find_loadout_parameter(value, "cond=");
+        parameter = strstr(value, "cond=");
         if (parameter)
-            condition = (float)atof(parameter);
+            condition = (float)atof(parameter + 5);
 
-        parameter = find_loadout_parameter(value, "ammo=");
+        parameter = strstr(value, "ammo=");
         if (parameter) {
-            const s32 parsed_ammo_count = atoi(parameter);
+            const s32 parsed_ammo_count = atoi(parameter + 5);
             ammo_count = parsed_ammo_count > 0 ? (u32)parsed_ammo_count : 0;
         }
 
-        parameter = find_loadout_parameter(value, "ammo_type=");
+        parameter = strstr(value, "ammo_type=");
         if (parameter)
-            ammo_type = atoi(parameter);
-
-        parameter = find_loadout_parameter(value, "drop_ammo=");
-        if (parameter) {
-            const s32 parsed_min = atoi(parameter);
-            s32 parsed_max = parsed_min;
-
-            LPCSTR separator = strchr(parameter, '-');
-            LPCSTR parameter_end = strchr(parameter, ',');
-
-            if (separator && (!parameter_end || separator < parameter_end))
-                parsed_max = atoi(separator + 1);
-
-            drop_ammo_min = parsed_min > 0 ? (u32)parsed_min : 0;
-            drop_ammo_max = parsed_max > 0 ? (u32)parsed_max : 0;
-
-            if (drop_ammo_max < drop_ammo_min)
-                std::swap(drop_ammo_min, drop_ammo_max);
-
-            custom_drop_ammo = true;
-        }
-
-        keep_ammo = has_loadout_flag(value, "keep_ammo");
+            ammo_type = atoi(parameter + 10);
 
         if (condition < 0.f)
             condition = 0.f;
         else if (condition > 1.f)
             condition = 1.f;
 
-        scope = has_loadout_flag(value, "scope");
-        silencer = has_loadout_flag(value, "silencer");
-        launcher = has_loadout_flag(value, "launcher");
+        scope = (NULL != strstr(value, "scope"));
+        silencer = (NULL != strstr(value, "silencer"));
+        launcher = (NULL != strstr(value, "launcher"));
     }
 
     bool is_empty() const { return 0 == xr_strcmp(section.c_str(), "none"); }
@@ -167,8 +97,6 @@ void CSE_ALifeObject::spawn_supplies(LPCSTR ini_string) {
     CInifile ini(&IReader((void*)(ini_string), xr_strlen(ini_string)),
                  FS.get_path("$game_config$")->m_Path);
 #pragma warning(pop)
-
-    xr_string death_ammo_metadata;
 
     for (u32 loadout_index = 1;; ++loadout_index) {
         string32 loadout_section;
@@ -245,10 +173,7 @@ void CSE_ALifeObject::spawn_supplies(LPCSTR ini_string) {
 
         CSE_ALifeItemWeapon* weapon = smart_cast<CSE_ALifeItemWeapon*>(entity);
 
-        if (!weapon)
-            continue;
-
-        if (!selected->ammo_count && !selected->custom_drop_ammo)
+        if (!weapon || !selected->ammo_count)
             continue;
 
         LPCSTR ammo_classes = weapon->m_caAmmoSections;
@@ -281,22 +206,6 @@ void CSE_ALifeObject::spawn_supplies(LPCSTR ini_string) {
         }
 
         weapon->ammo_type = (u8)ammo_type;
-
-        if (death_ammo_metadata.empty())
-            death_ammo_metadata = "[spawn_loadout_death_ammo]\n";
-
-        string256 metadata_line;
-
-        if (selected->keep_ammo) {
-            xr_sprintf(metadata_line, "%s = %s, keep\n", loadout_section, ammo_section);
-        } else if (selected->custom_drop_ammo) {
-            xr_sprintf(metadata_line, "%s = %s, custom, %u, %u\n", loadout_section,
-                       ammo_section, selected->drop_ammo_min, selected->drop_ammo_max);
-        } else {
-            xr_sprintf(metadata_line, "%s = %s, default\n", loadout_section, ammo_section);
-        }
-
-        death_ammo_metadata += metadata_line;
 
         for (u32 ammo_index = 0; ammo_index < selected->ammo_count; ++ammo_index) {
             CSE_Abstract* ammo = alife().spawn_item(ammo_section, o_Position, m_tNodeID,
@@ -361,26 +270,6 @@ void CSE_ALifeObject::spawn_supplies(LPCSTR ini_string) {
                         IItem->m_fCondition = f_cond;
                 }
             }
-        }
-    }
-
-    if (!death_ammo_metadata.empty()) {
-        LPCSTR current_ini = *m_ini_string;
-
-        if (!current_ini || !strstr(current_ini, "[spawn_loadout_death_ammo]")) {
-            xr_string updated_ini = current_ini ? current_ini : "";
-
-            if (!updated_ini.empty() && updated_ini[updated_ini.size() - 1] != '\n')
-                updated_ini += "\n";
-
-            updated_ini += death_ammo_metadata;
-
-            xr_delete(m_ini_file);
-            m_ini_string = updated_ini.c_str();
-        } else {
-            Msg("! [spawn_loadout_death_ammo] already exists for object [%s]; metadata was not "
-                "replaced",
-                name_replace());
         }
     }
 }
